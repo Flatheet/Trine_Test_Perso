60a6d7d36db19df45a78c8e9afb6763980947416
Damien Mathiotte <6-15455@saeinstitute.com>
2021-01-15 15:00:49 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR;
+
+public class GameManager : MonoBehaviour
+{
+    private string actualjoy;
+    private string _joy1 = "Joy1_";
+    private string _joy2 = "Joy2_";
+
+    [SerializeField] private GameObject[] _character;
+    
+    
+    [SerializeField] private bool _twoPlayer;
+
+    [SerializeField] private bool isGood;
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject b = Instantiate(_character[0], new Vector3(1, 1, 1), Quaternion.identity);
+        b.GetComponent<Player>().Actualjoy = _joy1;
+        if (_twoPlayer)
+        {
+            GameObject c = Instantiate(_character[0], new Vector3(-1, 1 - 1), Quaternion.identity);
+            c.GetComponent<Player>().Actualjoy = _joy2;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private string actualjoy;
+
+    [SerializeField]private string message = "Help";
+
+    public string Actualjoy
+    {
+        get => actualjoy;
+        set => actualjoy = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton(actualjoy + "Fire1"))
+            Debug.Log(message);
+    }
+}
