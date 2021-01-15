0e96df9c455b42481be51ccb5b70f88006fc8b69
Damien Mathiotte <6-15455@saeinstitute.com>
2021-01-15 15:17:16 +0100

    Initial commit

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,7 +7,7 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Lol");
+        Debug.Log("Loal");
     }
 
     // Update is called once per frame
